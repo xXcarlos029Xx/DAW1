e26bfa56220325498af9faefe99f8f4fbb91f2c3
Carlos <Carlos@DESKTOP-MO0NLHP>
2022-02-28 19:48:14 +0100

    Primera frase

---
+++ EjerGithub/src/ejergithub/EjerGithub.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejergithub;
+
+/**
+ *
+ * @author Carlos
+ */
+public class EjerGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("holo hala hele hili hulu");
+        int din=2;
+        int don=8;
+        int dindon=din+don;
+        System.out.println(dindon);
+    }
+    
+}
